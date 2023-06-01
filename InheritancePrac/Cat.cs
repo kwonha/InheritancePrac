51a8408b606456be2a25640b4aa7e7f51f56d853
Haeun Kwon <s2118@e-mirim.hs.kr>
2023-06-01 09:50:21 +0900

    상속관계에서 생성자

--- InheritancePrac/Cat.cs
+++ InheritancePrac/Cat.cs
@@ -4,6 +4,13 @@ namespace InheritancePrac
 {
     public class Cat : Animal
     {
+        public Cat()
+        {
+        }
+
+        public Cat(int age) : base(age)
+        {
+        }
 
         public void Meow()
         {
